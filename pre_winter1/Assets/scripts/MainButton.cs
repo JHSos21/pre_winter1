195e8f7b14c4ae98d87a51e84c5bfd7c9d7b09b5
JHSos21 <hyeonseong2190@gmail.com>
2026-01-19 11:00:37 +0900

    Main
    메인화면, 버튼 제작
    
    옵션과 하이스코어 미완성

---
+++ pre_winter1/Assets/scripts/MainButton.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour
+{
+    public void Road_InGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Option()
+    {
+        //옵션 창을 열기
+    }
+}
